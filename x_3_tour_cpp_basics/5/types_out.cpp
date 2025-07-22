43ad38a952422a5134d19b2fe9fb88f8645de914
mrrrTom <dagoroff@gmail.com>
2025-07-22 17:49:32 +0300

    3d section 5th exercise

---
+++ x_3_tour_cpp_basics/5/types_out.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+int main()
+{
+	bool flag = false;
+	char symbol = 'q';
+	int integerNumber = 1;
+	double doubleNumber = 33.2;
+	std::string word = "hello";
+	
+	std::cout << "bool: " << flag << std::endl;
+	std::cout << "char: " << symbol << std::endl;
+	std::cout << "int: " << integerNumber << std::endl;
+	std::cout << "double: " << doubleNumber << std::endl;
+	std::cout << "string: " << word << std::endl;
+	return 0;
+}
