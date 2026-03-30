bf24ad4a5186bea7c6910dcef3ab4a8c5d07a229
mrrrTom <dagoroff@gmail.com>
2026-03-30 19:04:00 +0300

    strp ch 8 ex 4 fixic

--- x_8_pointers_arrays_and_references/4/main.cpp
+++ x_8_pointers_arrays_and_references/4/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+
 int dif_positions(char* left, char* right) {
-	return (right - left);
+	int result = std::abs(right - left);
+	return result ? --result : result;
 }
 
 int main() {
 	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'j', 'k' };
 	
-	//char* r_ptr = arr + 3;
-	//std::cout << *r_ptr << std::endl;
+	char* r_ptr = arr + 3;
+	std::cout << *r_ptr << std::endl;
 	
-	std::cout << dif_positions(arr, arr);
+	std::cout << dif_positions(arr, r_ptr);
 	return 0;
 }
