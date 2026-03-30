89a69543d23a4a68a1782b3bbc185200de487331
mrrrTom <dagoroff@gmail.com>
2026-03-30 18:59:14 +0300

    strp ch 8 ex 4

--- x_8_pointers_arrays_and_references/4/main.cpp
+++ x_8_pointers_arrays_and_references/4/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+int dif_positions(char* left, char* right) {
+	return (right - left);
+}
 
 int main() {
+	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'j', 'k' };
+	
+	//char* r_ptr = arr + 3;
+	//std::cout << *r_ptr << std::endl;
+	
+	std::cout << dif_positions(arr, arr);
 	return 0;
 }
