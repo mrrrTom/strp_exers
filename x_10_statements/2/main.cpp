dbc1829a3b53745341a857f59fc35c43ae15756b
mrrrTom <dagoroff@gmail.com>
2026-04-13 12:02:39 +0300

    strp chp 10 ex 2

---
+++ x_10_statements/2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void f(int a, int b) {
+	if(a = 3) cout << "third" << endl;
+	if(a&077 == 0) cout << "casino" << endl;
+	a := b + 1;
+}
+
+int main() {
+	int a = 3, b = 1;
+	f(a, b);
+	return 0;
+}
+
+
