1d6fd4d22983ed2501c8099fae79b2c7f3119717
Daniil Gorbunov <dagoroff@gmail.com>
2026-08-26 17:55:28 +0300

    strp ex ch 13 funcs ex 8 init

---
+++ x_13_functions/8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct t_node {
+	string word;
+	int count;
+	t_node* left;
+	t_node* right;
+
+	t_node& insert(string new_word) {
+	}
+};
+
+int main (int argc, char *argv[]) {
+
+	return 0;
+}
