9f78a1cfc16570151cb4c2e3c1928eae3b813897
mrrrTom <dagoroff@gmail.com>
2026-09-03 20:14:35 +0300

    strp ex ch 13 funcs ex 16

--- x_13_functions/16/main.cpp
+++ x_13_functions/16/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 enum class lexic_state {
@@ -41,28 +42,39 @@ class macro_processor_fn {
 			switch(_cur_s) {
 				case static_cast<char>(lexems::sharp):
 					_state = lexic_state::pre_def;
+					cout << _cur_s;
 					break;
 				case static_cast<char>(lexems::slash):
 					_state = lexic_state::pre_comment;
+					cout << _cur_s;
 					break;
 				case static_cast<char>(lexems::w_space):
 				case static_cast<char>(lexems::n_line):
 				case static_cast<char>(lexems::tab):
+					cout << _cur_s;
 					break;
 				default:
+					_cur_symbs.push_back(_cur_s);
 					_state = lexic_state::var_can_be_used;
 					break;
 			}
 		}
 
 		void process_pre_def() {
+			cout << _cur_s;
 			switch(_cur_s) {
 				case static_cast<char>(lexems::w_space):
 				case static_cast<char>(lexems::tab):
 				{
 					if (_cur_symbs.size() == 0) break;
 					string cur_word {_cur_symbs.begin(), _cur_symbs.end()};
-					if (cur_word == _define_key_word) _state = lexic_state::init;
+					if (cur_word == _define_key_word) {
+						_state = lexic_state::init;
+					}
+					else {
+						_state = lexic_state::error;
+					}
+
 					_cur_symbs.clear();
 					break;
 				}
@@ -76,14 +88,168 @@ class macro_processor_fn {
 			}
 		}
 
+		string _cur_init_word;
+		bool has_wrong_word() {
+			char first_symb = _cur_symbs[0];
+			return ((first_symb < 'a') || (first_symb > 'z'));
+		}
+
 		void process_init() {
+			cout << _cur_s;
 			switch(_cur_s) {
 				case static_cast<char>(lexems::w_space):
-				case static_cast<char>(lexems::tab
+				case static_cast<char>(lexems::tab):
+					if (_cur_symbs.size() != 0) {
+						if (has_wrong_word()) {
+							_cur_symbs.clear();
+							_state = lexic_state::error;
+						 }
+						else {
+							_cur_init_word = string {_cur_symbs.begin(), _cur_symbs.end()};
+							_cur_symbs.clear();
+							_state = lexic_state::def;
+						}
+					}
+
+					break;
+				case static_cast<char>(lexems::n_line):
+					_cur_symbs.clear();
+					_cur_init_word = "";
+					_state = lexic_state::error;
+					break;
+				default:
+					_cur_symbs.push_back(_cur_s);
+					break;
+			}
+		}
+		
+		map<string, string> _defined_vars;
+		void process_def() {
+			cout << _cur_s;
+			switch(_cur_s) {
+				case static_cast<char>(lexems::n_line):
+					if (_cur_symbs.size() != 0) {
+						string new_val = string {_cur_symbs.begin(), _cur_symbs.end()};
+						_defined_vars.insert({_cur_init_word, new_val});
+						_cur_symbs.clear();
+						_cur_init_word = "";
+						_state = lexic_state::empty_line;
+					}
+					else {
+						_cur_symbs.clear();
+						_cur_init_word = "";
+						_state = lexic_state::error;
+
+					}
+
+					break;
+				default:
+					_cur_symbs.push_back(_cur_s);
+					break;
 			}
 		}
 
-		void process_def() {
+		void process_pre_comment() {
+			cout << _cur_s;
+			switch(_cur_s) {
+				case static_cast<char>(lexems::slash):
+					_state = lexic_state::single_line_comment;
+					break;
+				case static_cast<char>(lexems::star):
+					_state = lexic_state::multi_line_comment;
+					break;
+				default:
+					_state = lexic_state::error;
+					break;
+			}
+		}
+
+		void process_single_line_comment() {
+			cout << _cur_s;
+			switch(_cur_s) {
+				case static_cast<char>(lexems::n_line):
+					_state = lexic_state::empty_line;
+					break;
+				default:
+					break;
+			}
+		}
+
+		void process_multi_line_comment() {
+			cout << _cur_s;
+			switch(_cur_s) {
+				case static_cast<char>(lexems::star):
+					_state = lexic_state::pre_multi_line_comment_closure;
+					break;
+				default:
+					break;
+			}
+		}
+
+		void process_pre_multi_line_comment_closure() {
+			cout << _cur_s;
+			switch(_cur_s) {
+				case static_cast<char>(lexems::slash):
+					_state = lexic_state::var_can_be_used;
+					break;
+				case static_cast<char>(lexems::star):
+					break;
+				default:
+					_state = lexic_state::multi_line_comment;
+					break;
+			}
+		}
+
+		void process_var_can_be_used() {
+			switch(_cur_s) {
+				case static_cast<char>(lexems::tab):
+				case static_cast<char>(lexems::w_space): {
+					if (_cur_symbs.size() != 0) {
+						string cur_word = string {_cur_symbs.begin(), _cur_symbs.end()};
+						if (_defined_vars.find(cur_word) != _defined_vars.end()) {
+							cout << _defined_vars[cur_word];
+							cout << static_cast<char>(lexems::w_space);
+						}
+						else {
+							cout << cur_word << static_cast<char>(lexems::w_space);
+						}
+
+						_cur_symbs.clear();
+						_state = lexic_state::var_can_be_used;
+					}
+
+					break;
+				}
+				case static_cast<char>(lexems::n_line): {
+					if (_cur_symbs.size() != 0) {
+						string cur_word = string {_cur_symbs.begin(), _cur_symbs.end()};
+						if (_defined_vars.find(cur_word) != _defined_vars.end()) {
+							cout << _defined_vars[cur_word];
+							cout << static_cast<char>(lexems::w_space);
+						}
+						else {
+							cout << cur_word << static_cast<char>(lexems::n_line);
+						}
+_cur_symbs.clear();
+						_state = lexic_state::empty_line;
+					}
+
+					break;
+				}
+				default: {
+					if (_cur_symbs.size() != 0) {
+						string cur_word = string {_cur_symbs.begin(), _cur_symbs.end()};
+						if (_defined_vars.find(cur_word) != _defined_vars.end()) {
+							cout << _defined_vars[cur_word];
+							_cur_symbs.clear();
+						}
+					}
+					
+					_cur_symbs.push_back(_cur_s);
+					_state = lexic_state::var_can_be_used;
+					break;
+				}
+			}
 		}
 
 		void process(char cur_s) {
@@ -103,6 +269,21 @@ class macro_processor_fn {
 					break;
 				case lexic_state::def:
 					process_def();
+					break;
+				case lexic_state::pre_comment:
+					process_pre_comment();
+					break;
+				case lexic_state::single_line_comment:
+					process_single_line_comment();
+					break;
+				case lexic_state::multi_line_comment:
+					process_multi_line_comment();
+					break;
+				case lexic_state::pre_multi_line_comment_closure:
+					process_pre_multi_line_comment_closure();
+					break;
+				case lexic_state::var_can_be_used:
+					process_var_can_be_used();
 					break;
 			}
 		}
