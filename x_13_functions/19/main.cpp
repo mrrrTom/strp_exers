395e32297c0a5cbb060a5b8fe3bb235598b2c3f7
mrrrTom <dagoroff@gmail.com>
2026-09-05 17:26:54 +0300

    strp ex 19 init

---
+++ x_13_functions/19/main.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <map>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+enum class Kind : char {
+	name, number, end,
+	plus = '+', minus = '-', mul = '*', div = '/', print = ';',
+	assign = '=', lp = '(', rp = ')'
+};
+
+struct Token{
+	Kind kind;
+	string string_value;
+	double number_value;
+};
+
+int no_of_errors;
+
+double error(const string& s) {
+	no_of_errors++;
+	cerr << "error: " << s  << endl;
+	return 1;
+}
+
+map<string, double> table;
+string collect_cur_string(istream& is) {
+	char cur_symb;
+	string result;
+	while(is.get(cur_symb) && cur_symb != ' ' && cur_symb != '(') {
+		result += cur_symb;
+	}
+
+	if (cur_symb == '(') is.putback('(');
+	return result;
+}
+class Token_stream {
+	public:
+		Token_stream(istream& s) : ip{&s}, owns{false} { }
+		Token_stream(istream* p) : ip{p}, owns{true} { }
+		~Token_stream() { close(); }
+
+		Token get() {
+			char ch;
+
+			do {
+				if (!ip -> get(ch)) return ct = {Kind::end};
+			} while (ch != '\n' && isspace(ch));
+			
+			switch (ch) {
+				case 0:
+					return ct = {Kind::end};
+				case '\n':
+				case ';':
+					return ct = {Kind::print};
+				case '*':
+				case '/':
+				case '+':
+				case '-':
+				case '(':
+				case ')':
+				case '=':
+					return ct = {static_cast<Kind>(ch)};
+				case '0': case '1': case '2': case '3': case '4': case '5':
+				case '6': case '7': case '8': case '9': case '.':
+					ip -> putback(ch);
+					*ip >> ct.number_value;
+					ct.kind = Kind::number;
+					return ct;
+				default:
+					if (isalpha(ch)) {
+						ip -> putback(ch);
+						istream& tmp {cin};
+						ct.string_value = collect_cur_string(tmp);
+						ct.kind = Kind::name;
+						return ct;
+					}
+
+					error("bad token");
+					return ct = {Kind::print};
+			}
+		}
+	
+		const Token& current() { return ct; }
+		
+		void set_input(istream& s) { close(); ip = &s; owns = false; }
+		void set_input(istream* p) { close(); ip = p; owns = true; }
+	private:
+		void close() { if (owns) delete ip; }
+
+		istream* ip;
+		bool owns;
+		Token ct {Kind::end};
+};
+
+Token_stream ts {cin};
+double expr(bool);
+double sin(double inp) {
+	return std::sin(inp);
+}
+double log(double inp) {
+	return std::log(inp);
+}
+double sqrt(double inp) {
+	return std::sqrt(inp);
+}
+unordered_map<string, double (*)(double)> funcs { {"sin", sin}, {"log", log}, {"sqrt", sqrt}};
+double prim (bool get) {
+	if (get) ts.get();
+
+	switch (ts.current().kind) {
+		case Kind::number:
+			{
+				double v = ts.current().number_value;
+				ts.get();
+				return v;
+			}
+		case Kind::name:
+			{
+				string name_val = ts.current().string_value;
+				if (funcs.find(name_val) != funcs.end()) {
+					ts.get();
+					if (ts.current().kind != Kind::lp) return error("bad fuction call");
+					auto expr_result = expr(true);
+					ts.get();
+					if (ts.current().kind != Kind::rp) return error("bad function call");
+					ts.get();
+					return (funcs[name_val](expr_result));
+				}
+				else {
+					double& v = table[ts.current().string_value];
+					if (ts.get().kind == Kind::assign) v = expr(true);
+					return v;
+				}
+			}
+		case Kind::minus:
+			return -prim(true);
+		case Kind::lp:
+			{
+				auto e = expr(true);
+				if (ts.current().kind != Kind::rp) return error("')' expected");
+				ts.get();
+				return e;
+			}
+		default:
+			return error("primary expected");
+	}
+}
+double term(bool get) {
+	double left = prim(get);
+
+	for(;;) {
+		switch (ts.current().kind) {
+			case Kind::mul:
+				left *= prim(true);
+				break;
+			case Kind::div:
+				if (auto d = prim(true)) {
+					left /= d;
+					break;
+				}
+
+				return error("divide by 0");
+			default:
+				return left;
+		}
+	}
+}
+
+double expr(bool get) {
+	double left = term(get);
+	for(;;) {
+		switch (ts.current().kind) {
+			case Kind::plus:
+				left += term(true);
+				break;
+			case Kind::minus:
+				left -= term(true);
+				break;
+			default:
+				return left;
+		}
+	}
+}
+
+void calculate() {
+	for (;;) {
+		ts.get();
+		if (ts.current().kind == Kind::end) break;
+		if (ts.current().kind == Kind::print) continue;
+		cout << expr(false) << endl;
+	}
+}
+
+int main(void) {
+	table["pi"] = 3.1415926;
+	table["e"] = 2.71828;
+	calculate();
+
+	return no_of_errors;
+}
